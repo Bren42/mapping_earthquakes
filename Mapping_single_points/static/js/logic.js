2be3b4286abf7a6f77162589b9db1b88938bced7
Bren42 <nytech42@gmail.com>
2022-10-05 15:13:36 -0600

    added circle markers and changed background tile layer to the logic.js file in this branch

--- Mapping_single_points/static/js/logic.js
+++ Mapping_single_points/static/js/logic.js
@@ -5,13 +5,22 @@
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution:'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: api_key
 });
 
+//add a marker to the map
+//
+// var marker = L.marker([34.0522, -118.2437]).addTo(map);
 //then we need to add our 'graymap' tile layer to the map.
+
+L.circleMarker([34.0522, -118.2437], {
+   radius: 300,
+   fillColor: "Yellow",
+   color: "Black"
+}).addTo(map);
 
 streets.addTo(map);
 
