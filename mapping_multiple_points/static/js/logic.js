f1d805738b686f60be27d688b97e3beaa742316d
Bren42 <nytech42@gmail.com>
2022-10-06 13:07:24 -0600

    added mapping lines

--- mapping_multiple_points/static/js/logic.js
+++ mapping_multiple_points/static/js/logic.js
@@ -2,7 +2,30 @@
 // console.log("working");
 
 //create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// add GeoJSON data
+L.geoJSON(sanFranAirport).addTo(map);
 
 
 // Coordinates for each point to be used in the line.
