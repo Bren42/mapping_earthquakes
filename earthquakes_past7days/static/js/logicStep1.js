01619291939e8db42b4856e8d1f2ff7c16475567
Bren42 <nytech42@gmail.com>
2022-10-08 09:26:01 -0600

    adding updates to logicstep1.js with geojson earthquake data from usgs site

--- earthquakes_past7days/static/js/logicStep1.js
+++ earthquakes_past7days/static/js/logicStep1.js
@@ -14,38 +14,20 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 })
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/Bren42/mapping_earthquakes/Mapping_geoJSON_polygons/torontoNeighborhoods.json";
 
-
-// Create a style for the lines.
-let myStyle = {
-    color: "blue",
-    fillColor: "Yellow",
-    weight: 1
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data,{
-      style:myStyle,
-      onEachFeature: function(feature,layer) {
-          layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-      }
-      })
-        .addTo(map);
-});
-
-// streets.addTo(map);
